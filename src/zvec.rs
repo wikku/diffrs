a549da2fc4881c4325186e4f1b05dd9259ebbb40
Wiktor Kuchta <35867657+wiktorkuchta@users.noreply.github.com>
2023-02-06 17:21:50 +0100

    Optimize ZVec

--- src/zvec.rs
+++ src/zvec.rs
@@ -1,21 +1,10 @@
 use std::ops::{Index, IndexMut};
 
-pub struct ZVec<T>(pub Vec<T>);
-
-fn nat_of_int(int: isize) -> usize {
-    ((int * 2).abs() - (if int < 0 { 1 } else { 0 })).try_into().unwrap()
-}
-
-/*
-fn int_of_nat(nat: usize) -> isize {
-    let inat: isize = (nat / 2).try_into().unwrap();
-    if nat % 2 == 0 { inat } else { -inat }
-}
-*/
+pub struct ZVec<T>(pub Vec<T>, usize);
 
 impl<T> ZVec<T> {
     pub fn with_diam(diam: usize, value: T) -> Self where T: Clone {
-        ZVec(vec![value; 2*diam+1])
+        ZVec(vec![value; 2*diam+1], diam)
     }
 }
 
@@ -24,38 +13,12 @@ impl<T> Index<isize> for ZVec<T> {
     type Output = T;
 
     fn index(&self, int: isize) -> &Self::Output {
-        &self.0[nat_of_int(int)]
+        &self.0[(int + self.1 as isize) as usize]
     }
 }
 
 impl<T> IndexMut<isize> for ZVec<T> {
     fn index_mut(&mut self, int: isize) -> &mut Self::Output {
-        &mut self.0[nat_of_int(int)]
+        &mut self.0[(int + self.1 as isize) as usize]
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_nat_of_int() {
-        assert_eq!(nat_of_int(0), 0);
-        assert_eq!(nat_of_int(-1), 1);
-        assert_eq!(nat_of_int(1), 2);
-        assert_eq!(nat_of_int(-2), 3);
-        assert_eq!(nat_of_int(2), 4);
-    }
-
-    /*
-    #[test]
-    fn test_inverse() {
-        for i in -10..10 {
-            assert_eq!(int_of_nat(nat_of_int(i)), i);
-        }
-        for i in 0..21 {
-            assert_eq!(nat_of_int(int_of_nat(i)), i);
-        }
-    }
-    */
 }
